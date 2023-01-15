971b361cd41864ca6fdf97707f99a522bffec1e4
michb0t <t.pham29@live.com.au>
2023-01-15 17:24:10 +1100

    js updates
    refactored functions, solved taskCount functions

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-
-
 //-----TOGGLE MODE-------// 
 const toggleSwitch = document.querySelector(".theme-switch-wrapper")
 const body = document.querySelector("body")
@@ -35,66 +33,140 @@ function darkMode() {
 toggleSwitch.addEventListener("click",setTheme)
 
 
-
-
-// ----HANDLE INPUT ---- //
+// ----HANDLE TO DO INPUT ---- //
+const list = document.querySelector(".list")
 
 const toDoText = document.querySelector(".todo-text")
-const list = document.querySelector(".list")
-toDoText.addEventListener("change", createToDo)
-const toDoButtons = document.querySelectorAll(".checkBtn")
-const taskli = document.querySelectorAll(".task")
+let doneBtns = document.querySelectorAll(".checkBtn")
 const noTasksLeft = document.querySelector("#noTasksLeft")
+const remove = document.querySelectorAll(".remove")
 
-let tasks =[]
+let tasks = [];
+let activeTasks = [];
+let completedTasks = [];
 
+toDoText.addEventListener("change", createToDo)
+
+//CREATE TODO 
 function createToDo(e) {
-    let newToDo = e.target.value
-    list.innerHTML += `
-        <li class="task">
-            <button class="checkBtn">          
-                <img 
-                id="checkIcon" 
-                src="images/icon-check.svg" 
-                alt="check-icon"/>
-            </button>
-            <span class="toDoText">${newToDo}</span>
-            <img alt="deleteBtn" class="deleteBtn" src="images/icon-cross.svg">
-        </li>`
-    e.target.value = ""
-    tasks.push(newToDo)
-    taskAmount()
-   noTasksNote() 
+    let toDoVal = e.target.value
+    if (toDoVal == null || toDoVal == " ") {
+        alert("Please enter a task")
+    } else {
+        const toDo = document.createElement("li")
+        toDo.classList.add("task")
+        toDo.innerHTML += `
+                <button class="checkBtn">          
+                    <img 
+                    id="checkIcon" 
+                    src="images/icon-check.svg" 
+                    alt="check-icon"/>
+                </button>
+                <span class="toDoText break-word">${toDoVal}</span>
+                <img alt="remove" class="remove" src="images/icon-cross.svg">
+                `
+        list.appendChild(toDo)
+        e.target.value = ""
+        tasks.length +=1 
+        bindRemove(toDo.querySelector(".remove"))
+        bindDone(toDo.querySelector(".checkBtn"))
+        noTasksNote();
+        updateTaskCount();
+
+    }
 }
 
-// STRIKE THROUGH IF COMPLETE
+//bind remove and done functions to the buttons 
 
+const bindRemove = (btn) => {
+    btn.addEventListener("click", () => {
+        const mainPar = btn.parentElement
+        mainPar.remove()
+        tasks.length -=1
+        noTasksNote()
+        updateTaskCount();
+    })
+}
 
+const bindDone = (btn) => {
+    const checkIcon = btn.querySelector("#checkIcon")
+    btn.addEventListener("click", () => {
+        const mainPar = btn.parentElement
+        btn.classList.toggle("checked")
+        checkIcon.style.display = "block"
+        mainPar.classList.toggle("completed")
+    })
+}
 
-//REMOVE IF CLICKED
-let deleteBtns = document.querySelectorAll(".deleteBtn")
+remove.forEach(bindRemove)
+doneBtns.forEach(bindDone)
 
-list.addEventListener("click", (e) => {
-    if(e.target.classList.value === "deleteBtn") {
-        e.target.parentNode.remove() 
-        tasks.length -= 1
+//HANDLE COMPLETED TASKS 
+
+const clearComplete = document.querySelector("#clr-completed")
+
+clearComplete.addEventListener("click", () => {
+    const allTasks = document.querySelectorAll(".task")
+    for (let task of allTasks) {
+        if (task.classList.contains("completed")) {
+            task.remove()
+            tasks.length -=1
+            updateTaskCount();
+        }
     }
-    taskAmount()
     noTasksNote();
 })
 
-function noTasksNote() {
-    noTasksLeft.style.display = tasks.length >= 1 
-    ? "none" 
-    : "block"
-}
-// ITEMS LEFT
+
+
+
+//---- HANDLE ITEMS LEFT-----//
 
 const itemsLeft = document.querySelector("#items-left")
 
 
-function taskAmount() {
-    itemsLeft.innerHTML = tasks.length === 0 
-    ? "0 items left"
-    : `${tasks.length} items left`
+
+let updateTaskCount = () => {
+    const allTasks = document.querySelectorAll(".task")
+    console.log(allTasks.length)
+    itemsLeft.textContent = `${allTasks.length} items left`
 }
+
+const noTasksNote = () => {
+    noTasksLeft.style.display = tasks.length === 0
+    ? "block" 
+    : "none"
+}
+
+
+
+
+// tasks.length = 0
+// for (let task of allTasks) {
+//     console.log(task)
+//     if (task.classList.contains("completed")) {
+//     task.remove()
+//     }}
+// itemsLeft.innerHTML = `${tasks.length} items left`
+// noTasksNote();
+//     itemsLeft.innerHTML = tasks.length === 0 
+//     ? "0 items left"
+//     : `${tasks.length} items left`
+// }
+
+
+// CLEAR COMPLETED
+
+
+//HANDLE COMPLETED
+
+// add a commpleted class to the task, 
+//if user clicks on "completed", show only completed tasks (display.none method)
+// !=completed, show the task 
+
+
+
+// tomorrow
+//fix the inputwidth, and do a flex wrap if the task goes over. (consider max char limit?)
+
+// add more padding to lengfthy tasks
